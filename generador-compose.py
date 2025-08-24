173fc4bf862a3306e586d22f116d2857764396e5
MateoF01 <mateofernandez380@gmail.com>
2025-08-24 12:17:03 -0300

    fix error

--- generador-compose.py
+++ generador-compose.py
@@ -16,7 +16,7 @@ for i in range(1, int(n_clientes) + 1):
       - CLI_ID={i}
       - CLI_LOG_LEVEL=DEBUG
     volumes:
-      - ./server/config:/config:ro
+      - ./client/config.yaml:/config.yaml:ro
     networks:
       - testing_net
     depends_on:
@@ -36,7 +36,7 @@ services:
       - PYTHONUNBUFFERED=1
       - LOGGING_LEVEL=DEBUG
     volumes:
-      - ./client/config:/config:ro
+      - ./server/config.ini:/config.ini:ro
     networks:
       - testing_net
 
