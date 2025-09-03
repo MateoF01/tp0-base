b159d6852f1df71e3342dca957c96bdd64e8cfe1
MateoF01 <mateofernandez380@gmail.com>
2025-09-03 09:11:05 -0300

    Update serializer.py

--- server/common/serializer.py
+++ server/common/serializer.py
@@ -1,11 +1,12 @@
 from common.utils import Bet
 
 DELIM = "|"
+PARTS_NUMBER = 6
 
 def deserialize_bet(data: bytes) -> Bet:
 
     parts = data.decode("utf-8").split(DELIM)
-    if len(parts) != 6:
+    if len(parts) != PARTS_NUMBER:
         raise ValueError(f"Invalid payload, expected 6 fields: {data}")
 
     return Bet(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5])
